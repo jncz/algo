b603bfc5b2ec9cf5cb0ee935df4df95ca6a9d6c6
jncz <li.j2ee@gmail.com>
2016-01-11 16:25:14 +0800

    init checkin

---
+++ src/com/spss/test/IntegerCompareTest.java
@@ -0,0 +1,164 @@
+/************************************************************************
+** IBM Confidential
+** 
+** OCO Source Materials
+**
+** IBM SPSS Analytic Catalyst
+**
+** (C) Copyright IBM Corp. 2016
+**
+** The source code for this program is not published or otherwise divested of its trade secrets, 
+** irrespective of what has been deposited with the U.S. Copyright Office.
+************************************************************************/
+package com.spss.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegerCompareTest {
+
+  @Test
+  public void test_Integer_autobox_m129() {
+    Integer i1 = -129;
+    Integer i2 = -129;
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m128() {
+    Integer i1 = -128;
+    Integer i2 = -128;
+    
+    assertTrue(i1 == i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m127() {
+    Integer i1 = -127;
+    Integer i2 = -127;
+    
+    assertTrue(i1 == i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_0() {
+    Integer i1 = 0;
+    Integer i2 = 0;
+    
+    assertTrue(i1 == i2);
+  }
+
+  @Test
+  public void test_Integer_autobox_127() {
+    Integer i1 = 127;
+    Integer i2 = 127;
+    
+    assertTrue(i1 == i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_128() {
+    Integer i1 = 128;
+    Integer i2 = 128;
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m129_wrapper() {
+    Integer i1 = Integer.valueOf(-129);
+    Integer i2 = Integer.valueOf(-129);
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m128_wrapper() {
+    Integer i1 = Integer.valueOf(-128);
+    Integer i2 = Integer.valueOf(-128);
+    
+    assertTrue(i1 == i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m127_wrapper() {
+    Integer i1 = Integer.valueOf(-127);
+    Integer i2 = Integer.valueOf(-127);
+    
+    assertTrue(i1 == i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_0_wrapper() {
+    Integer i1 = Integer.valueOf(0);
+    Integer i2 = Integer.valueOf(0);
+    
+    assertTrue(i1 == i2);
+  }
+
+  @Test
+  public void test_Integer_autobox_127_wrapper() {
+    Integer i1 = Integer.valueOf(127);
+    Integer i2 = Integer.valueOf(127);
+    
+    assertTrue(i1 == i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_128_wrapper() {
+    Integer i1 = Integer.valueOf(128);
+    Integer i2 = Integer.valueOf(128);
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m129_constructor() {
+    Integer i1 = new Integer(-129);
+    Integer i2 = new Integer(-129);
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m128_constructor() {
+    Integer i1 = new Integer(-128);
+    Integer i2 = new Integer(-128);
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_m127_constructor() {
+    Integer i1 = new Integer(-127);
+    Integer i2 = new Integer(-127);
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_0_constructor() {
+    Integer i1 = new Integer(0);
+    Integer i2 = new Integer(0);
+    
+    assertTrue(i1 != i2);
+  }
+
+  @Test
+  public void test_Integer_autobox_127_constructor() {
+    Integer i1 = new Integer(127);
+    Integer i2 = new Integer(127);
+    
+    assertTrue(i1 != i2);
+  }
+  
+  @Test
+  public void test_Integer_autobox_128_constructor() {
+    Integer i1 = new Integer(128);
+    Integer i2 = new Integer(128);
+    
+    assertTrue(i1 != i2);
+  }
+}
---
+++ src/com/spss/test/JosephusProblem.java
@@ -0,0 +1,90 @@
+/************************************************************************
+ ** IBM Confidential
+ ** 
+ ** OCO Source Materials
+ **
+ ** IBM SPSS Analytic Catalyst
+ **
+ ** (C) Copyright IBM Corp. 2016
+ **
+ ** The source code for this program is not published or otherwise divested of its trade secrets,
+ * irrespective of what has been deposited with the U.S. Copyright Office.
+ ************************************************************************/
+package com.spss.test;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author liping
+ *
+ *         In the Josephus problem from antiquity, N people are in dire straits and agree to the
+ *         following strategy to reduce the population. They arrange themselves in a circle (at
+ *         positions numbered from 0 to N-1) and proceed around the circle, eliminating every Mth
+ *         person until only one person is left. Legend has it that Josephus figured out where to
+ *         sit to avoid being eliminated.
+ */
+public class JosephusProblem {
+
+  private static int peopleNum;
+  private static int eliminatedStep;
+  private static int currentRemoveIdx = -1;
+
+  private static LinkedList<Integer> q = new LinkedList<Integer>();
+  
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    peopleNum = scan.nextInt();//1--N
+    eliminatedStep = scan.nextInt();//0--(N-1)
+    
+    paramCheck();
+    
+    solution();
+    scan.close();
+  }
+
+  private static void paramCheck() {
+    if(peopleNum < 1){
+      throw new IllegalArgumentException("People number should bigger than 1");
+    }
+    
+    if(eliminatedStep > peopleNum - 1 || eliminatedStep < 0){
+      throw new IllegalArgumentException("eliminatedStep should be 0--(N-1)");
+    }
+  }
+
+  private static void solution() {
+    initQueue();
+    eliminate();
+  }
+
+  private static void eliminate() {
+    if(q.size() == 1){
+      return;
+    }
+
+    if(currentRemoveIdx == -1){
+      currentRemoveIdx = eliminatedStep;
+    }
+    int a = currentRemoveIdx - q.size();
+    if(a >= 1){
+      //int b = currentRemoveIdx % q.size();
+      currentRemoveIdx = eliminatedStep - a;
+    }
+    Integer people = q.remove(currentRemoveIdx);
+    System.out.print(people+" ");
+    currentRemoveIdx += (eliminatedStep+1);
+    eliminate();
+  }
+
+  private static void initQueue() {
+    for(int i=0;i<peopleNum;i++){
+      q.add(i);
+    }
+  }
+
+}
